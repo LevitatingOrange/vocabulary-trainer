558bca08d6eed6a8f8e572caba686ccbd3334475
Lennart Vogelsang <dev@vogelsangs.net>
2016-03-18 21:58:54 +0100

    new branch; will restructure the app here

---
+++ src/View.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="view">
+	<div class="header-bar">
+	    <div class="header-label">
+		{{ label }}
+	    </div>
+	    <div class="header-info">
+		{{ info }}
+	    </div>
+	</div>
+	<div class="content">
+	    <slot name="content"></slot>
+	</div>
+	<div class="footer-bar">
+	    <slot name="footer"</slot>
+	</div>
+    </div>
+</template>
+<script>
+ export default {
+     props: {
+	 label: String,
+	 info: String,
+	 
+     }
+ }
+</script>
